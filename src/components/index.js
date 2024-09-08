cb02f21f4383b97221bc72f501774cd446742717
Bulat G <gazizullinbr@yandex.ru>
2024-09-08 16:18:31 +0300

    в процессе настройки валидации форм

--- src/components/index.js
+++ src/components/index.js
@@ -12,9 +12,13 @@ const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonAdd = document.querySelector('.profile__add-button');
 
 const formElement = document.forms['edit-profile'];
-const formAdd = document.forms['new-place'];
 const nameInput = formElement.elements['name'];
 const jobInput = formElement.elements['description'];
+
+const formAdd = document.forms['new-place'];
+const placeNameInput = formAdd.elements['place-name'];
+const linkInput = formAdd.elements['link'];
+
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
@@ -71,8 +75,8 @@ formElement.addEventListener('submit', handleProfileFormSubmit);
 function addCard(evt) {
   evt.preventDefault();  
   
-  const placeNameInput = formAdd.elements['place-name'];
-  const linkInput = formAdd.elements['link'];
+  // const placeNameInput = formAdd.elements['place-name'];
+  // const linkInput = formAdd.elements['link'];
   const objectCards = {};
 
   objectCards.name = placeNameInput.value;
@@ -96,3 +100,80 @@ function openModalImage(name, link) {
   imagePopup.src = link.src;
   popupCaption.textContent = name.textContent;
 }
+
+// --------------------------
+
+// показать ошибку
+function showInputError(formElement, inputElement, errorMessage) {
+  const inputError = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  inputError.classList.add('popup__input-error_active');
+  inputError.textContent = errorMessage;
+} 
+
+// скрыть ошибку
+function hideInputError(formElement, inputElement) {  
+  const inputError = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  inputError.classList.remove('popup__input-error_active');
+  inputError.textContent = '';
+} 
+
+
+const regexInputText = /[а-яёa-z\s\-]/gi;
+
+// проверяем валидность поля ввода
+function isValid(formElement, inputElement) {
+  if (!inputElement.validity.valid && !regexInputText.test(inputElement.value)) {
+    showInputError(formElement, inputElement, inputElement.dataset.errorMessage);
+  } else if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// проверяем наличие невалидного поля
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid
+  })
+}
+
+// переключаем активность кнопки 
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add('popup__button_inactive');
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove('popup__button_inactive');
+  }
+}
+
+function setEventListener(formElement) {  
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {      
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    }); 
+  })
+}
+
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+  formList.forEach((formElement) => {
+    setEventListener(formElement);
+  })
+}
+
+enableValidation();
+
+
